38c6b75d988131bca72cf81a22e9a2027d0301f9
sonjiseokk <thswltjr11@gmail.com>
2023-10-18 20:44:25 +0900

    Initial commit

---
+++ src/main/java/com/freetec/freetecspringbootaws2/FreetecSpringbootAws2Application.java
@@ -0,0 +1,13 @@
+package com.freetec.freetecspringbootaws2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreetecSpringbootAws2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreetecSpringbootAws2Application.class, args);
+	}
+
+}
