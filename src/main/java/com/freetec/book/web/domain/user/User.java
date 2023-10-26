1925807ab043a88fc665b178991861eaf4b900f0
sonjiseokk <thswltjr11@gmail.com>
2023-10-26 19:00:39 +0900

    :fix Add User entity pk key

--- src/main/java/com/freetec/book/web/domain/user/User.java
+++ src/main/java/com/freetec/book/web/domain/user/User.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity(name = "users")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
